048e02bf36499a326f21c7484e1eebb03f8922ee
Noah Davey <noah.davey@gmail.com>
2020-09-07 08:03:49 +1000

    Setup eslint

--- buildscripts/srcServer.js
+++ buildscripts/srcServer.js
@@ -2,7 +2,9 @@ import express from 'express';
 import path from 'path';
 import open from 'open';
 import webpack from 'webpack';
-import config from '../webpack.config';
+import config from '../webpack.config.dev';
+
+/* eslint-disable no-console */
 
 const port = 3000;
 const app = express();
@@ -14,7 +16,7 @@ app.use(require('webpack-dev-middleware')(compiler, {
 }))
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '../src/index.html'));
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
 app.listen(port, function(err) {
