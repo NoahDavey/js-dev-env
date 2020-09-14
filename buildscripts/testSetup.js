ada09e8986a9f8156e891940b8f76856f68cc91c
Noah Davey <noah.davey@gmail.com>
2020-09-14 14:28:24 +1000

    Testing & CI/CD Added

---
+++ buildscripts/testSetup.js
@@ -0,0 +1,5 @@
+//Require babel to transpile before our tests run
+require('@babel/register')();
+
+//Disable webpack features that mocha doesn't understand
+require.extensions['.css'] = function() {};
